f2efdce50cdac0ec9308dec14f9eaefed17d21c3
wlc <wlc606@gmail.com>
2023-08-18 23:16:11 +0800

    更新deploy

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import eslintPlugin from 'vite-plugin-eslint'
+import { fileURLToPath, URL } from 'node:url'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    eslintPlugin({
+      include: ['src/**/*.js', 'src/**/*.vue', 'src/*.js', 'src/*.vue']
+    })
+  ],
+  base: process.env.NODE_ENV === 'production' ? '/vue_winter_week8_v4/' : '/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
+})
